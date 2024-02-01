0efccec7f98902966622ba8863519cf3055de303
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-02-01 20:29:22 +0000

    my files index.html, style.css and testing.js

--- testing.js
+++ testing.js
@@ -1,43 +1,91 @@
-// Define songTitle here. This should be the name of the song that the user is searching for.
-var songTitle = "Despacito";
+const $inputArtist = $('.input-artist');
+const $inputSong = $('.input-song');
+const $buttonFind = $('.find-btn');
+const $buttonView = $('.view-btn');
+const $songDesc = $('.song-desc');
+const $lyricField = $('.lyric-text');
+const $errorBox = $('.error-box');
 
-const musixmatchApiKey = 'b5c14c721405ef563497ac0b168300a7';
-const youtubeApiKey = 'AIzaSyC_A54f2YKjbw7wjMVH79Abs1rVj4PfNH4';
+// function handleButtonClick() {
+//   getLyrics();
+//   getVideo();
+// }
 
-var songs = [""];
-var songStorage = JSON.parse(localStorage.getItem("songs")) || [];
-
-var url = 'https://www.googleapis.com/youtube/v3/search?' + 
- 'part=snippet&' + 
- 'maxResults=1&' + 
- 'q=' + encodeURIComponent(songTitle) + '&' + 
- 'key=' + youtubeApiKey;
-
-fetch(url)
- .then(response => response.json())
- .then(data => {
-    var videoId = data.items[0].id.videoId;
-    // Now that we have the video ID, we can use it to play the video
-    document.querySelector('#youtubePlayer').setAttribute('src', 'https://www.youtube.com/embed/' + videoId);
- })
- .catch((error) => {
-    console.error('Error:', error);
- });
-
-fetch(`http://localhost:3000/proxy/ws/1.1/track.search?q_track=${songTitle}&apikey=${musixmatchApiKey}`)
- .then(response => response.json())
- .then(data => {
-     var trackId = data.message.body.track_list[0].track.track_id;
+$buttonFind.on('click', getLyrics);
  
-     // Then, get the lyrics of the song
-     return fetch(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=${musixmatchApiKey}`);
- })
- .then(response => response.json())
- .then(data => {
-     var lyrics = data.message.body.lyrics.lyrics_body;
-     // Now that we have the lyrics, we can display them
-     $('#lyrics').text(lyrics);
- });
+function getLyrics() {
+    let artistVal = $inputArtist.val();
+    let songVal = $inputSong.val();
 
- 
+    if (artistVal !== '' && songVal !== '') {
+        $errorBox.hide();
+        fetch(`https://api.lyrics.ovh/v1/${artistVal}/${songVal}`)
+        .then(response => response.json())
+        .then(lyric => {
+            let song = `${artistVal}-${songVal}`.split('-');
+            let artistName = song[0].split(' ').map(elem => elem[0].toUpperCase() + elem.slice(1)).join(' ');
+            let songName = song[1].split(' ').map(elem => elem[0].toUpperCase() + elem.slice(1)).join(' ');
+    
+            $songDesc.text(`${artistName} - ${songName}`);
+            
+            let lyrics = lyric.lyrics.split("\n").join("<br />");
+            
+            $lyricField.html(lyrics);
+    
+            $inputArtist.val('');
+            $inputSong.val('');
+        })
+        .catch(err => {
+            $errorBox.show();
+            $errorBox.children().eq(1).text('Sorry! Lyrics not found.');
+            setTimeout(() => {
+                $errorBox.hide();
+            }, 2500);
+        });
 
+    } else {
+        $errorBox.show();
+        setTimeout(() => {
+            $errorBox.hide();
+        }, 2500);
+    }
+}
+$buttonView.on('click', getVideo);
+function getVideo()
+ {
+ const youtubeApikey = 'AIzaSyCxq1fOqiEee1Wk6c7izIOl5YX9YwZwDUE'
+ let artist = $inputArtist.val();
+ let song = $inputSong.val();
+ let query = `${artist} ${song}`;
+ fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=${encodeURIComponent(query)}&key=${youtubeApikey}`)
+.then(response => response.json())
+.then(data => {
+    let videoId = null;
+    for (let i = 0; i < data.items.length; i++) {
+      if (data.items[i].snippet.title.toLowerCase().includes(query.toLowerCase())) {
+          videoId = data.items[i].id.videoId;
+          break;
+      }
+    
+}
+if (videoId !== null) {
+    console.log('Found video: ', videoId);
+} else {
+    console.log('No exact match found');
+}
+
+    
+    // Create an iframe
+    let $iframe = $('<iframe/>', {
+      width: '560',
+      height: '315',
+      src: `https://www.youtube.com/embed/${videoId}`,
+      frameborder: '0',
+      allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share',
+      allowfullscreen: ''
+  });
+    
+    // Append the iframe to your page
+    $('#youtubePlayer').append($iframe);
+});
+}
