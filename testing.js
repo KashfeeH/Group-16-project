f531c021b8a352a4493de51fed790d991a625b08
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-02-06 22:18:27 +0000

    latest javascript and index files

--- testing.js
+++ testing.js
@@ -5,6 +5,8 @@ const buttonView = $('.view-btn');
 const songtitle = $('.song-title');
 const lyricField = $('.lyric-text');
 const errorBox = $('.error-box');
+let search = [""];
+var searchStorage = JSON.parse(localStorage.getItem("search")) || []
 
 buttonFind.on('click', findLyrics);
 function findLyrics() {
@@ -20,14 +22,18 @@ function findLyrics() {
             return response.json();
         })
         .then(lyric => {
+            localStorage.setItem(`lyrics-${artistVal}-${songVal}`, lyric.lyrics);
             let songDetails = `${artistVal}-${songVal}`.split('-');
+            let listItem = $('<li>').addClass('list-group-item').text(songDetails);
+            $('#recentSearchList').prepend(listItem);
             let artistName = songDetails[0].split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
             let songName = songDetails[1].split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-            songtitle.text(`${artistName} - ${songName}`);
+            songtitle.text(`${artistName}${songName}`);
             let formattedLyrics = lyric.lyrics.split("\n").slice(1).join("<br />");
             lyricField.html(formattedLyrics);
             inputArtist.val('');
             inputSong.val('');
+            localStorage.setItem(`lyrics-${artistVal}-${songVal}`, lyric.lyrics);
         })
         .catch(error => {
             errorBox.show();
@@ -38,7 +44,6 @@ function findLyrics() {
     }
 }
 
-
 buttonView.on('click', getVideo);
 function getVideo()
 {
@@ -46,7 +51,7 @@ const youtubeApikey = 'AIzaSyCxq1fOqiEee1Wk6c7izIOl5YX9YwZwDUE'
 let artist = inputArtist.val().trim();
 let song = inputSong.val().trim();
 let query = `${artist} ${song}`;
-fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=${encodeURIComponent(query)}&key=${youtubeApikey}`)
+fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&key=${youtubeApikey}`)
 .then(response => response.json())
 .then(data => {
     let videoId = null;
@@ -81,3 +86,4 @@ allowfullscreen: ''
     $('#youtubePlayer').append($iframe);
 });
 }
+
