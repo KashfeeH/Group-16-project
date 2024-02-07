320bf248b75f9f2925f257d3a1ab7369174d1879
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-02-07 17:08:29 +0000

    updated files

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const buttonView = $('.view-btn');
 const songTitle = $('.song-title');
 const lyricField = $('.lyric-text');
 const errorBox = $('.error-box');
-let search = [""];
+var search = [""];
 var searchStorage = JSON.parse(localStorage.getItem("search")) || []
 
 buttonFind.on('click', findLyrics);
 function formatSongDetails(artist, song) {
-let songDetails = `${artist}-${song}`.split('-');
-let artistName = songDetails[0].split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-let songName = songDetails[1].split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+var songDetails = `${artist}-${song}`.split('-');
+var artistName = songDetails[0].split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+var songName = songDetails[1].split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 return { artistName, songName };
 }
 
@@ -21,14 +21,14 @@ return lyrics.split("\n").slice(1).join("<br />");
 }
 
 function updateUIWithLyrics(lyrics, artist, song) {
-let { artistName, songName } = formatSongDetails(artist, song);
+var { artistName, songName } = formatSongDetails(artist, song);
 songTitle.text(`${artistName}-${songName}`);
 lyricField.html(formatLyrics(lyrics));
 }
 
 function findLyrics() {
-let artistVal = inputArtist.val().trim();
-let songVal = inputSong.val().trim();
+var artistVal = inputArtist.val().trim();
+var songVal = inputSong.val().trim();
 if (artistVal !== '' && songVal !== '') {
     errorBox.hide();
     fetch(`https://api.lyrics.ovh/v1/${artistVal}/${songVal}`)
@@ -39,10 +39,10 @@ if (artistVal !== '' && songVal !== '') {
         return response.json();
     })
     .then(lyric => {
-        let listItem = $('<li>').addClass('list-group-item').text(`${artistVal}-${songVal}`);
+        var listItem = $('<li>').addClass('list-group-item').text(`Lyrics: ${artistVal}.${songVal}`);
         $('#recentSearchList').prepend(listItem);
         listItem.on('click', function() {
-            let storedLyrics = localStorage.getItem(`lyrics-${artistVal}-${songVal}`);
+            var storedLyrics = localStorage.getItem(`lyrics-${artistVal}-${songVal}`);
             if (storedLyrics) {
                 // Display the lyrics from local storage
                 updateUIWithLyrics(storedLyrics, artistVal, songVal);
@@ -71,14 +71,14 @@ buttonView.on('click', getVideo);
 function getVideo()
 {
 const youtubeApikey = 'AIzaSyCxq1fOqiEee1Wk6c7izIOl5YX9YwZwDUE'
-let artist = inputArtist.val().trim();
-let song = inputSong.val().trim();
-let query = `${artist} ${song}`;
+var artist = inputArtist.val().trim();
+var song = inputSong.val().trim();
+var query = `${artist} ${song}`;
 fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&key=${youtubeApikey}`)
 .then(response => response.json())
 .then(data => {
-let videoId = null;
-for (let i = 0; i < data.items.length; i++) {
+var videoId = null;
+for (var i = 0; i < data.items.length; i++) {
 if (data.items[i].snippet.title.toLowerCase().includes(query.toLowerCase())) {
     videoId = data.items[i].id.videoId;
     console.log("videoId", videoId)
@@ -91,19 +91,19 @@ if (videoId !== null) {
 console.log('Found url: ', videoId);
 
 // Update the search history in local storage
-let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 searchHistory.unshift({ artist, song, videoId });
 localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 
 // Prepend the search term to the recent search list
-let listItem = $('<li>').addClass('list-group-item').text(`${artist}-${song}`);
+var listItem = $('<li>').addClass('list-group-item').text(`Video: ${artist}${song}`);
 $('#recentSearchList').prepend(listItem);
 localStorage.setItem('videoId', videoId);
 listItem.on('click', function() {
     var videoId = localStorage.getItem('videoId');
     if (videoId) {
         // Create an iframe
-        let $iframe = $('<iframe/>', {
+        var $iframe = $('<iframe/>', {
             width: '560',
             height: '315',
             src: `https://www.youtube.com/embed/${videoId}`,
@@ -127,7 +127,7 @@ console.log('No match found');
 $('#youtubePlayer').empty();
 
 //Create an iframe
-let $iframe = $('<iframe/>', {
+var $iframe = $('<iframe/>', {
 width: '560',
 height: '315',
 src:`https://www.youtube.com/embed/${videoId}`,
